107f89c5ba332067335feda07d64078548f1a1ea
srandria <>
2025-06-17 16:18:15 +0200

    updated

---
+++ srcs/requirements/ftp/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Lire le mot de passe depuis le fichier (optionnel)
+[ -f "$FTP_PASSWORD_FILE" ] && FTP_PASSWORD=$(cat "$FTP_PASSWORD_FILE")
+
+# Création de l'utilisateur FTP
+useradd -m "$FTP_USER" && echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
+# Création des répertoires nécessaires
+mkdir -p /var/www/wordpress /var/run/vsftpd/empty
+chown -R "$FTP_USER:$FTP_USER" /var/www/wordpress
+chown -R "$FTP_USER:$FTP_USER" /var/www/
+chown -R "$FTP_USER:$FTP_USER" /var/www/static
+
+# Lancer vsftpd
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
+
